34edf818b9c361bc8bcfcf41272c957ee562b9f6
Carlos Padilla <konnylizamaya@gmail.com>
2021-11-18 10:51:19 -0600

    anadiendo rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// importando rutas
+import { RouterModule } from '@angular/router';
+import { ROUTES } from './app.routes';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { ArtistaComponent } from './components/artista/artista.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+
+
 
 @NgModule({
   declarations: [
@@ -16,7 +22,8 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
     NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES, { useHash: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
